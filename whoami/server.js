12a63857fd79fa375d603bebca88fc576604ad37
saichand <itsmesaichand8@gmail.com>
2017-07-06 00:29:56 +0530

    Update server.js

--- whoami/server.js
+++ whoami/server.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var url = require('url');
 
+//adds views to middleware calls index.html automatically on calling
+// app.use(express.static('views'));
+//adds public to middleware
 app.use(express.static('public'));
 
+
 app.get("*", function (req, res) {
+
   var ip = req.headers['x-forwarded-for'].split(',')[0];
   var lang = req.headers["accept-language"].split(',')[0];
   var os = req.headers["user-agent"].split(/\)|\(/)[1];
-  console.log(ip, lang, os);
-  
   var user = {ipaddress : ip,
               language : lang,
               software : os
@@ -19,6 +22,7 @@ app.get("*", function (req, res) {
   res.send(user);
 });
 // listen for requests :)
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
